8879b97721f658a6f06c8352f138732883648e66
Willem de Groot <gwillem@gmail.com>
2017-01-27 09:31:24 +0100

    Change --newonly to scan everything but report only new infections (+caching of rulesets) (#44)
    * Change set syntax for Py2.6 compat
    * Refactored into settings file
    * Add caching for rulesets and whitelists
    * Fix semver syntax err in setup.py
    * Fix requests 0.8.4 compatibility
    * Add hypernode-mwscan entry

---
+++ mwscan/settings.py
@@ -0,0 +1,10 @@
+import os
+import logging
+
+CACHEDIR = os.path.expanduser('~/.cache/mwscan')
+LAST_RUN_FILE = os.path.join(CACHEDIR, 'last_run')
+DEFAULT_RULES_FILE = os.path.join(os.path.dirname(__file__), 'data', 'all-confirmed.yar')
+CODE_EXT = ('php', 'phtml', 'js', 'jsx', 'html', 'php3', 'php4', 'php5', 'php7', 'sh')
+
+logging.basicConfig(format='[*] %(message)s', level=logging.DEBUG)
+logging.getLogger('requests').setLevel(logging.WARNING)
