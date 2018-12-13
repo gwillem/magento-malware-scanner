31bf15aa3d4c587776f8dbc8a8cedd3223d9c296
Jonas Hünig <huenig@t-online.de>
2018-12-13 17:57:40 +0100

    Added whitelist for https://github.com/slackero/phpwcms/tree/master/include/inc_module/mod_ads/inc which is an CMS

---
+++ corpus/whitelisted/phpwcms/include/inc_module/mod_ads/inc/ads_fe_init.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * phpwcms content management system
+ *
+ * @author Oliver Georgi <og@phpwcms.org>
+ * @copyright Copyright (c) 2002-2018, Oliver Georgi
+ * @license http://opensource.org/licenses/GPL-2.0 GNU GPL-2
+ * @link http://www.phpwcms.org
+ *
+ **/
+
+// some mod ADS functions only needed in frontend, but done right after opening page
+// script contains everything necessary to track ad banner clicks and so on...
